bb174f6082d6e1a79343038d4afa3e9d36abd39c
hayoun <fishingest@kookmin.ac.kr>
2022-06-30 15:28:25 +0900

    Fix setting

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
-# from config.settings import STATIC_ROOT
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -159,7 +157,6 @@ MEDIA_URL = '/media/'
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 ROOT_DIR=os.path.dirname(BASE_DIR)
-STATIC_ROOT=os.path.join(ROOT_DIR, '.static_root')
 # settings.py
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
